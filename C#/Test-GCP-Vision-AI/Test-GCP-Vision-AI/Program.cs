6045422dd4e2655e31618f0d6c8ac64ebf48709c
Alex Kim <akim8@emich.edu>
2019-09-24 10:29:44 -0400

    add C# vision ai test, reconfigure gitignores

---
+++ C#/Test-GCP-Vision-AI/Test-GCP-Vision-AI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Google.Cloud.Vision.V1;
+
+
+namespace Test_GCP_Vision_AI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates a client
+            var client = ImageAnnotatorClient.Create();
+
+            // Load the image file into memory
+            Console.WriteLine("Enter the path to an image");
+            string filePath = Console.ReadLine();
+            var image = Image.FromFile(filePath);
+
+            // Performs label detection on the image file
+            var response = client.DetectLabels(image);
+
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
+        }
+    }
+}
---
+++ C#/Test-GCP-Vision-AI/readme.md
@@ -0,0 +1,12 @@
+# Test GCP Vision AI
+
+[This is just some code from the Vision AI docs to test out the library with C#.](https://cloud.google.com/vision/docs/libraries)
+
+To run this project, you must have a GCP service account key set as the
+GOOGLE_APPLICATION_CREDENTIALS environment variable in your command line.
+
+This was supposed to be then transfered over to a Unity project for use with AR
+Foundation, but Unity doesn't have support for NuGet and the alternatives ways
+to import the library were not working, so this implementation of machine vision
+has been scrapped.
+
